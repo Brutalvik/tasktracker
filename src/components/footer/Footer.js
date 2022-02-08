83e368514c685cc85763ebb0282b63dc272d715b
vikram <vikcanada90@gmail.com>
2022-02-07 20:33:37 -0500

    Added Gradient Footer Button

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../header/Header.css'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <button className="btn-grad">ADD</button>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = ({ title }) => {
   return (
-    <div className="header-container">
+    <div className="container">
       <h1 className="title">{ title }</h1>
     </div>
   )
