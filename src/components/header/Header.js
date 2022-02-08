70de71220102ca31f8c0f8588f3a90639159d34d
vikram <vikcanada90@gmail.com>
2022-02-07 20:18:42 -0500

    Project Start - Intial Commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = ({ title }) => {
+  return (
+    <div className="header-container">
+      <h1 className="title">{ title }</h1>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+export default Header
